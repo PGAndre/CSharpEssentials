cd07701bd9af8201fce46482428e8e788af75c9b
Andrey Pryahin <apryahin@gmail.com>
2020-05-07 05:22:21 +0300

    Lesson16_operators_object

---
+++ Lesson_16_2/Block.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_16_2
+{
+    class Block
+    {
+        public int Side1 { get; set; }
+        public int Side2 { get; set; }
+        public int Side3 { get; set; }
+        public int Side4 { get; set; }
+
+        public Block(int side1, int side2,int side3,int side4)
+        {
+            Side1 = side1;
+            Side2 = side2;
+            Side3 = side3;
+            Side4 = side4;              
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Block block)
+            {
+                return (Side1 == block.Side1) && (Side2 == block.Side2) && (Side3 == block.Side3) && (Side4 == block.Side4);
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public override string ToString()
+        {
+            return "Параметры блока: " + Side1 + " * " + Side2 + " * " + Side3 + " * " + Side4;
+        }
+
+        public override int GetHashCode()
+        {
+            return Side1 ^ Side2 ^ Side3 ^ Side4;
+        }
+    }
+}
---
+++ Lesson_16_2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_16_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Block block1 = new Block(1, 2, 3, 4);
+            Block block2 = new Block(2, 3, 1, 4);
+
+
+            Console.WriteLine(block1.ToString());
+            Console.WriteLine(block2.ToString());
+            Console.WriteLine(block1.Equals(block2));
+            
+
+            if (block1.GetHashCode() == block2.GetHashCode())
+            {
+                Console.WriteLine("Параметры блоков равны");
+            }
+            else
+            {
+                Console.WriteLine("Параметры блоков не равны");
+            }
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
