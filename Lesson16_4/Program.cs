1d6e666993c5e2e4db353545eaafd19b33cbe4b6
Andrey Pryahin <apryahin@gmail.com>
2020-05-08 05:41:44 +0300

    Lesson16_end

---
+++ Lesson16_4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson16_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Timing time1 = new Timing(DateTime.Now);
+            Timing time2 = new Timing(new DateTime(2019, 05, 07));
+            int daysDiff = time1 - time2;
+            Console.WriteLine(time1.Time.Month);
+            Console.WriteLine(daysDiff);
+
+            Timing newdate = Timing.PlusDays(time1, 1);
+            Console.WriteLine(newdate);
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lesson16_4/Timing.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson16_4
+{
+    class Timing
+    {
+        private DateTime time;
+        public DateTime Time
+        {
+            get
+            {
+                return time;
+            }
+            set
+            {
+                time = value;
+            }
+        }
+
+        public Timing(DateTime time)
+        {
+            this.time = time;
+        }
+
+        public static int operator -(Timing time1, Timing time2)
+        {
+            TimeSpan timespan = time1.Time - time2.Time;
+            return timespan.Days;
+        }
+
+        public static Timing PlusDays(Timing time1, int days)
+        {
+            DateTime newDate = time1.Time.AddDays(days);
+            return new Timing(newDate);
+        }
+
+        public override string ToString()
+        {
+            return "Date: " + time;
+        }
+
+
+
+
+    }
+}
